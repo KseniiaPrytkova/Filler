a3b41672e32c1af72bfc410cabce430b1f85aa0d
KseniiaPrytkova <prytkovakseniia@gmail.com>
2019-09-19 20:14:33 +0200

    add perfect gnl for purposes of current proj

--- libft/get_next_line_fl.c
+++ libft/get_next_line_fl.c
@@ -1,7 +1,32 @@
-
 #include "libft.h"
 
-char	*string_sub(char *s, unsigned int start, size_t len)
+char	*join_me(char *s1, char const *s2)
+{
+	size_t	s1_len;
+	size_t	s2_len;
+	char	*result_string;
+
+	s1_len = 0;
+	s2_len = 0;
+	result_string = NULL;
+	if (s1 != NULL && s2 != NULL)
+	{
+		s1_len = ft_strlen(s1);
+		s2_len = ft_strlen(s2);
+		result_string = ft_strnew(s1_len + s2_len + 1);
+		if (result_string)
+		{
+			ft_strncpy(result_string, s1, s1_len);
+			free(s1);
+			ft_strncpy((result_string + s1_len), s2, s2_len);
+		}
+		else
+			return (NULL);
+	}
+	return (result_string);
+}
+
+static char	*string_sub(char *s, unsigned int start, size_t len)
 {
 	char	*substring;
 	size_t	string_len;
@@ -28,48 +53,47 @@ char	*string_sub(char *s, unsigned int start, size_t len)
 	return (substring);
 }
 
+void set_line(char **result_string, char **line)
+{
+	int str_len;
+	int counter;
+
+	str_len = ft_strlen(*result_string);
+	counter = 0;
+	while ((*result_string)[counter] != '\n' && (*result_string)[counter] != '\0')
+	{
+		counter++;
+	}
+	*line = ft_strnew(counter);
+	*line = ft_strncpy(*line, *result_string, counter);
+
+	*result_string = string_sub(*result_string, counter,
+	 						(str_len - counter));
+}
+
 int get_next_line_fl(const int fd, char **line)
 {
 	static char *result_string;
-	char buffer[BUFF_SIZE+1];
-	int read_bytes = 0;
+	char 		buffer[BUFF_SIZE + 1];
+	int 		read_bytes;
 
-	if (fd < 0 || fd > MAX_FILES || BUFF_SIZE < 0 || line == NULL)
+	read_bytes = 0;
+	if (fd < 0 || BUFF_SIZE < 0 || line == NULL)
 		return (-1);
 	if (!result_string)
 		result_string = ft_strnew(1);
 	while (!(ft_strchr(result_string, '\n')))
 	{
-		read_bytes = read(fd, buffer, BUFF_SIZE);
-		if (read_bytes == -1)
+		if ((read_bytes = read(fd, buffer, BUFF_SIZE)) < 0)
 			return (-1);
 		buffer[read_bytes] = '\0';
-		result_string = ft_strjoin(result_string, buffer);
-		if (read_bytes == 0)
-		{
-			if (*result_string == '\0')
-				return (0);
-			else
-				break;
-		}
+		//result_string = ft_strjoin(result_string, buffer);
+		result_string = join_me(result_string, buffer);
+		if (*result_string == '\0' && read_bytes == 0)
+			return (0);
+		else if (read_bytes == 0)
+			break;
 	}
-
-	int str_len;
-
-	str_len = ft_strlen(result_string);
-
-	int counter = 0;
-	while (*result_string != '\n' && *result_string != '\0')
-	{
-		result_string++;
-		counter++;
-	}
-	result_string -= counter;
-	*line = ft_strnew(counter);
-	*line = ft_strncpy(*line, result_string, counter);
-
-	result_string = string_sub(result_string, counter,
-							(str_len - counter));
-
+	set_line(&result_string, line);
 	return (1);
 }
