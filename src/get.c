324bac27ab2614efc78c4cc6057a3fb14efb9f40
KseniiaPrytkova <prytkovakseniia@gmail.com>
2019-10-10 05:44:52 +0200

    refactoring

--- src/get.c
+++ src/get.c
@@ -1,23 +1,47 @@
 #include "filler.h"
 
-int read_the_map(t_init *initial, int n, char board[][n])
+
+void read_the_map_loop_set(t_init *initial, int j, int i, int *catch)
+{
+	initial->opp_x_curr = j;
+	initial->opp_y_curr = i;
+	*catch = 1;
+}
+
+void read_the_map_loop(t_init *initial, int n, char board[][n])
 {
 	int i;
 	int j;
 	int catch;
-	char *str = NULL;
+    char *str;
 
-	i = 0;
-	j = 0;
+	i = -1;
 	catch = 0;
+	str = NULL;
+	while (++i < initial->y_plateau)
+	{
+		get_next_line_fl(0, &str);
+		char *line = ft_strsplit(str, ' ')[1];
+		j = -1;
+		while (++j < initial->x_plateau)
+		{
+			board[j][i] = line[j];
+			if (((board[j][i] == initial->enemy_figure) ||
+				(board[j][i] == initial->enemy_figure - 32)) && !catch)
+				read_the_map_loop_set(initial, j, i, &catch);
+		}
+		ft_strdel(&str);
+	}
+}
 
+int read_the_map(t_init *initial, int n, char board[][n])
+{
+	char *str;
+
+	str = NULL;
 	get_next_line_fl(0, &str);
 	if (str == NULL)
-	{
-		// a null string from the filter vm
-		// ends the game.
 		return (0);
-	}
 	else if (str[0] == 'P' && str[1] == 'l')
 	{
 		// If line is "Plateau ..." here.
@@ -25,32 +49,9 @@ int read_the_map(t_init *initial, int n, char board[][n])
 		get_next_line_fl(0, &str);
 	}
 	else if (str[0] != ' ' && str[1] != ' ' && str[2] != ' ')
-	{
 		return (0);
-	}
 	ft_strdel(&str);
-	// Read board data line by line into
-	// board array.
-	while (i < initial->y_plateau)
-	{
-		get_next_line_fl(0, &str);
-		char *line = ft_strsplit(str, ' ')[1];
-		int j = 0;
-		while (j < initial->x_plateau)
-		{
-			board[j][i] = line[j];
-			if (((board[j][i] == initial->enemy_figure) || (board[j][i] == initial->enemy_figure - 32)) && !catch)
-			{
-				initial->opp_x_curr = j;
-				initial->opp_y_curr = i;
-				catch = 1;
-			}
-			j++;
-		}
-		ft_strdel(&str);
-		i++;
-	}
-
+	read_the_map_loop(initial, n, board);
 	return (1);
 }
 
